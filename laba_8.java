98a480d4d8350727c735ace977f8a15ff9b21b89
Konstantinov Eduard <keduard2003@mail.ru>
2023-12-15 17:37:21 +0900

    lab 8 changed

--- laba_8.java
+++ laba_8.java
@@ -1,13 +1,18 @@
 public class laba_8 {
     public static void main(String[] args) {
         People people =  new People("Алина","Петрова",17);
+        System.out.println(people);
         people.walk();
         people.run();
         people.voice();
+
         Student student = new Student("Анна","Томская",19);
+        System.out.println(student);
         student.walk();
         student.learn();
+
         Teacher teacher = new Teacher("Олег","Васильев",36);
+        System.out.println(teacher);
         teacher.voice();
         teacher.teach();
     }
